56f11d4cfbf2aab785521ff926d40b954fcacc33
khaledSabry-dev <khaled.sabry@paysky.io>
2025-09-02 16:37:06 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,40 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-import { SidenavComponent } from "./sidenav.component";
+import { SidenavComponent } from './sidenav.component';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { HeaderComponent } from './header.component';
 import { RevenueTrendsComponent } from './revenue-trends.component';
 import { DailyTransactionsComponent } from './daily-transactions.component';
 import { MetricCardComponent } from './metric-card/metric-card.component';
+import { SmartSideBarComponent } from 'ngx-dashboard-kit';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidenavComponent, NzAvatarModule, NzBadgeModule, HeaderComponent, RevenueTrendsComponent, DailyTransactionsComponent, MetricCardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SmartSideBarComponent,
+    NzAvatarModule,
+    NzBadgeModule,
+    HeaderComponent,
+    RevenueTrendsComponent,
+    DailyTransactionsComponent,
+    MetricCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   showLayout = true;
   constructor(private router: Router) {
-    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: any) => {
-      // Hide layout for /login and any child routes
-      this.showLayout = !/^\/login($|\/)/.test(event.urlAfterRedirects);
-    });
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        // Hide layout for /login and any child routes
+        this.showLayout = !/^\/login($|\/)/.test(event.urlAfterRedirects);
+      });
   }
 }
